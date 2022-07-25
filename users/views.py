75e1d88321063af4dad7462398e77c87747e9201
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-25 16:32:28 -0400

    rename files

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def loginUser(req):
-    return render(req,'root/pages/main.html')
+    return render(req, 'root/layout/main.html')
