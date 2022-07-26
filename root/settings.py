7858309b9fb5f4fb83ffcbf0744fc7a2d7eaadf2
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-07-25 20:29:04 -0400

    remore js and img folder

--- root/settings.py
+++ root/settings.py
@@ -132,13 +132,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 # STATIC_URL = 'static/'
 # Configure Django App for Heroku.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
 django_heroku.settings(locals())
 
 # Default primary key field type
